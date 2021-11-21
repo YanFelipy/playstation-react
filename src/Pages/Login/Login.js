d39e21d75605db48abaff53c00e9fbd6644beeea
YanFelipy22 <yan-felipy@hotmail.com>
2021-11-20 21:49:27 -0300

    Open close modal

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,14 +1,23 @@
-import React from 'react';
 import Form from '../../Components/Form';
 import { Link } from "react-router-dom";
 import PsLogo from '../../assets/ps-logo.svg';
 import Bg from './ps-bg.jpg';
 import { Button, Modal } from 'react-bootstrap';
 import './Login.css';
+import { useState } from 'react';
+
 
 
 const Login = () => {
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+
   return (
+
     <>
       <div className="bg-box">
         <img className="bg" src={Bg} alt="Playstation " />
@@ -24,7 +33,8 @@ const Login = () => {
               Entrar
             </Link>
 
-            <Button className="btn btn-dark b-gradient btn-lg my-4 text-white btn btn-primary" data-bs-toggle="modal" id="btToggle" data-bs-target="#exampleModal" data-bs-whatever="">
+            <Button className="btn btn-dark b-gradient btn-lg my-4 text-white btn btn-primary"
+              data-bs-toggle="modal" id="btToggle" data-bs-target="#exampleModal" data-bs-whatever="" onClick={handleShow}>
               Inscreva-se
             </Button>
           </div>
@@ -32,51 +42,52 @@ const Login = () => {
 
       </div>
       <div>
-        <Modal show={true} className="modal fade my-5" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+
+        <Modal show={show} className="modal fade my-5" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
           <div className="modal-dialog">
             <div className="modal-content">
 
               <Modal.Header>
                 <h5 className="modal-title" id="exampleModalLabel">Inscreva-se na Playstation</h5>
-                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" onClick={handleClose} aria-label="Close"></button>
               </Modal.Header>
 
               <Modal.Body>
                 <form>
-                 
+
                   <div className="form-floating mb-3">
-  <input type="text" className="form-control" id="floatingName" placeholder="Name" />
-  <label htmlFor="floatingPassword">Nome</label>
-</div>
-<div className="form-floating mb-3 ">
-  <input  value="" type="text" className="form-control" id="floatingSecondName" placeholder="Name" />
-  <label htmlFor="floatingPassword">Sobrenome</label>
-</div>
-                 
-                  <div className="mb-3">
-                  <div className="form-floating mb-3">
-                  <input type="email" className="form-control" id="floatingInputValue" placeholder="name@example.com" />
-                <label htmlFor="floatingInputValue">Email:</label>
+                    <input type="text" className="form-control" id="floatingName" placeholder="Name" />
+                    <label htmlFor="floatingPassword">Nome</label>
                   </div>
+                  <div className="form-floating mb-3">
+                    <input value="" type="text" className="form-control" id="floatingSecondName" placeholder="Name" />
+                    <label htmlFor="floatingPassword">Sobrenome</label>
                   </div>
 
- 
-<div className="mb-3 d-flex ">
-                  <div className="form-floating mb-3 col-6">
-  <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
-  <label htmlFor="floatingPassword">Senha</label>
-</div>
-<div className="form-floating mb-3 col-6">
-  <input type="password" className="form-control" id="floatingConfirmPassword" placeholder="Password" />
-  <label htmlFor="floatingConfirmPassword">Confirme a senha</label>
-</div>
-</div>
+                  <div className="mb-3">
+                    <div className="form-floating mb-3">
+                      <input type="email" className="form-control" id="floatingInputValue" placeholder="name@example.com" />
+                      <label htmlFor="floatingInputValue">Email:</label>
+                    </div>
+                  </div>
+
+
+                  <div className="mb-3 d-flex ">
+                    <div className="form-floating mb-3 col-6">
+                      <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                      <label htmlFor="floatingPassword">Senha</label>
+                    </div>
+                    <div className="form-floating mb-3 col-6">
+                      <input type="password" className="form-control" id="floatingConfirmPassword" placeholder="Password" />
+                      <label htmlFor="floatingConfirmPassword">Confirme a senha</label>
+                    </div>
+                  </div>
                 </form>
               </Modal.Body>
 
-              <Modal.Footer>
-                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" className="btn btn-primary">Send message</button>
+              <Modal.Footer className="d-flex align-items-center justify-content-center">
+                <Button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={handleClose}>Fechar</Button>
+
               </Modal.Footer>
             </div>
           </div>
