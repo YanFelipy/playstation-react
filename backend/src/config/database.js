92ab2a69754e2217d9ffd2915779c348fd1f49bd
YanFelipy22 <yan-felipy@hotmail.com>
2021-12-13 03:56:15 -0300

    first migration sequelize

---
+++ backend/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: '24080231',
+    database: 'psreact-postgres',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+}
